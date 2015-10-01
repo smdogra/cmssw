9e1650bc51d1f63fcc4a5cd2c13525ca635e2d5e
smdogra <smdogra@yahoo.co.in>
2015-10-01 16:39:32 -0300

    Update RecoHiCentrality_EventContent_cff.py

--- RecoHI/HiCentralityAlgos/python/RecoHiCentrality_EventContent_cff.py
+++ RecoHI/HiCentralityAlgos/python/RecoHiCentrality_EventContent_cff.py
@@ -2,15 +2,18 @@ import FWCore.ParameterSet.Config as cms
 
 RecoHiCentralityFEVT = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*',
+                                           'keep *_centralityBin_*_*',
                                            'keep recoClusterCompatibility*_hiClusterCompatibility_*_*')
     )
 
 RecoHiCentralityRECO = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*',
+                                           'keep *_centralityBin_*_*', 
                                            'keep recoClusterCompatibility*_hiClusterCompatibility_*_*')
     )
 
 RecoHiCentralityAOD = cms.PSet(
     outputCommands = cms.untracked.vstring('keep recoCentrality*_hiCentrality_*_*',
+                                           'keep *_centralityBin_*_*', 
                                            'keep recoClusterCompatibility*_hiClusterCompatibility_*_*')
     )
